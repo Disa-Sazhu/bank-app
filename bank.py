6897200ee1fdec739c0f2c1cf5d111f26b87b8f5
Thayaparan Disalini <UT010735@unicomtic.com>
2025-05-05 13:37:53 +0530

    first commit

---
+++ bank.py
@@ -0,0 +1,70 @@
+print("======== Admin Log In=======\n")
+U_name="Author"
+U_Password="Author123"
+Username=input("Enter the Username :")
+Password=input("Enter the Password :")
+
+#==================================================================
+print("========Admin Menu========")
+print("1. Create Customer")
+print("2. Create Account")
+print("3.Deposit")
+print("4. Withdraw")
+print("5. Transfer Money")
+print("6. Transaction History")
+print("7. Change Intrest rate")
+print("8. Update Accounts Details")
+print("9. Logout")
+
+
+choice=("Enter a number from 1-7")
+if choice==1:
+    def Create Customer()
+elif choice==2:
+    def Create Account()
+elif choice==3:
+    def Deposit()
+elif choice==4:
+    def Withdraw()
+elif choice==5:
+    def Transfer Money()
+elif choice==6:
+    def Transaction History()
+elif choice==7:
+    def Change Intrest rate()
+elif choice==8:
+    def Update Accounts Details()
+elif choice==9:
+    print("Thanks for using the service.")
+    break
+else:
+    print("Invalid choice.")
+
+#====================================================================
+print("========Customer Menu========")
+print("1. Deposit")
+print("2. Withdraw")
+print("3. Transfer Money")
+print("4. Transaction History")
+print("5. View Interest rate")
+print("6. View All Accounts")
+print("7. Logout")
+
+choice=("Enter a number from 1-7")
+if choice==1:
+    def Deposit()
+elif choice==2:
+    def Withdraw()
+elif choice==3:
+    def Transfer Money()
+elif choice==4:
+    def Transaction History()
+elif choice==5:
+    def View Intrest rate()
+elif choice==6:
+    print("Thanks for using the service.")
+    break
+else:
+    print("Invalid choice.")
+
+#=========================================================
